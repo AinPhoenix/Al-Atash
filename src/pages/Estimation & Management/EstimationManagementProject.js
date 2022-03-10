0b0e5e963bfadf83d041fe615d12927d3a1cc910
AinPhoenix <MuhammadAliIrtaza@gmail.com>
2022-03-10 12:56:47 +0500

    Added Services Images

--- src/pages/Estimation & Management/EstimationManagementProject.js
+++ src/pages/Estimation & Management/EstimationManagementProject.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const EstimationManagementProject = () => {
-  return <div>EstimationManagementProject</div>;
+  let { projectId } = useParams();
+  return <div>{projectId} EstimationManagementProject</div>;
 };
 
 export default EstimationManagementProject;
