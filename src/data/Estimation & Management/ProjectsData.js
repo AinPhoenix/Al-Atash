f5b547134de994c668e6ab2142376f420d9650ce
Malik Muneeb <malikmuneeb20@hotmail.com>
2022-03-19 17:42:24 +0500

    ch

--- src/data/Estimation & Management/ProjectsData.js
+++ src/data/Estimation & Management/ProjectsData.js
@@ -1,4 +1,15 @@
 import BOQ1 from "../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-01.jpg";
+import BOQ2 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-07.jpg';
+import BOQ3 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-08.jpg';
+import BOQ4 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-12.jpg';
+import BOQ5 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-13.jpg';
+import BOQ6 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-15.jpg';
+import BOQ7 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-23.jpg';
+import BOQ8 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-28.jpg';
+import BOQ9 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-36.jpg';
+import BOQ10 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-38.jpg';
+import BOQ11 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-43.jpg';
+import BOQ12 from '../../images/Estimation & Management/Project/01-6 Marla Villa Dha Multan/DHAM-6 MARLA-VOL-III BOQ-44.jpg';
 
 const ProjectsData = () => {
   return [
@@ -13,7 +24,7 @@ const ProjectsData = () => {
       buildingArea: '6 Marla',
       unit: 10,
       projectType: 'Villa',
-      pictures: [BOQ1],
+      pictures: [BOQ1,BOQ2,BOQ3,BOQ4,BOQ5,BOQ6,BOQ7,BOQ8,BOQ9,BOQ10,BOQ11,BOQ12],
       price: 6000000.0,
     },
     {
--- src/pages/Estimation & Management/EstimationManagementProject.js
+++ src/pages/Estimation & Management/EstimationManagementProject.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ProjectsFunction from '../../functions/Estimation & Management/ProjectsFunction';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination"
+import "../../components/Estimation & Management/Home/style.css";
 
 const EstimationManagementProject = () => {
   let { projectId } = useParams();
@@ -21,10 +24,24 @@ const EstimationManagementProject = () => {
     }
   };
 
+const automateslider = () => {
+  var returnArr=[];
+  for (let i = 0; i < data.pictures.length; i++) {
+    if(i==0){
+   returnArr.push(<SwiperSlide  style={{width:"400px",height:"500px"}}>  <img  style={{width:"400px",height:"auto"}} src={data.pictures[i]}/>     </SwiperSlide>);
+    }
+    else{
+      returnArr.push(<SwiperSlide  style={{width:"400px",height:"500px"}}>  <img  style={{width:"400px",height:"auto"}} src={data.pictures[i]}/>    </SwiperSlide>);
+    }
+  }
+  return returnArr;
+}
+
   return (
     <div>
+      <div>
       <div style={{height:"70vh",width:"100%"}}>
-          <img style={{height:"100%",width:"100%",objectFit:"cover"}} src={data.pictures}/>
+          <img style={{height:"100%",width:"100%",objectFit:"cover"}} src={data.pictures[0]}/>
       </div>
       <div style={{display:"flex",height:"30vh",justifyContent:"space-evenly",flexDirection:"column",padding:"5vh 9vh"}}>
         <div style={{fontSize:"5vh"}}>
@@ -36,6 +53,21 @@ const EstimationManagementProject = () => {
         <div>
        {priceComp()}
        </div>
+      </div>
+      </div>
+
+      {/* SLider */}
+      <div style={{padding:"6vh"}}>
+      <Swiper slidesPerView="auto" style={{padding:"0 30px 0"}} className={`mySwiper`}>
+       
+       {automateslider()}
+       </Swiper>
+      </div>
+<div style={{padding:"3vh"}}></div>
+      {/* box */}
+      <div style={{display:"flex",padding:"10vh  15vh",flexDirection:"column",justifyContent:"space-between",color:"#FFFFFF",width:"100%",height:"50vh",background: "#191D19 0% 0% no-repeat padding-box"}}>
+          
+      
       </div>
     </div>
   );
