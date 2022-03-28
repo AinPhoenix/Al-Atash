95c38cbd457fa3685b7be18551856702c9726238
AinPhoenix <MuhammadAliIrtaza@gmail.com>
2022-03-28 21:49:12 +0500

    Client Images Uploaded

--- src/data/ClientsData.js
+++ src/data/ClientsData.js
@@ -1,4 +1,4 @@
-import DHA from "./../images/Estimation & Management/DHA.png";
+import DHA from './../images/Estimation & Management/DHA.png';
 
 const ClientsData = () => {
   return [
@@ -7,33 +7,33 @@ const ClientsData = () => {
       img: DHA,
     },
     {
-        key: 2,
-        img: DHA,
-      },
-      {
-        key: 3,
-        img: DHA,
-      },
-      {
-        key: 4,
-        img: DHA,
-      },
-      {
-        key: 5,
-        img: DHA,
-      },
-      {
-        key: 6,
-        img: DHA,
-      },
-      {
-        key: 7,
-        img: DHA,
-      },
-      {
-        key: 8,
-        img: DHA,
-      },
+      key: 2,
+      img: DHA,
+    },
+    {
+      key: 3,
+      img: DHA,
+    },
+    {
+      key: 4,
+      img: DHA,
+    },
+    {
+      key: 5,
+      img: DHA,
+    },
+    {
+      key: 6,
+      img: DHA,
+    },
+    {
+      key: 7,
+      img: DHA,
+    },
+    {
+      key: 8,
+      img: DHA,
+    },
   ];
 };
 
