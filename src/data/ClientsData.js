bdb05c10f41433a9e9eb3e77efea4d0c1e9e6f03
Malik Muneeb <malikmuneeb20@hotmail.com>
2022-03-29 23:13:18 +0500

    clients data

--- src/data/ClientsData.js
+++ src/data/ClientsData.js
@@ -1,38 +1,159 @@
-import DHA from './../images/Estimation & Management/DHA.png';
-
+import c1 from '../images/Estimation & Management/Clients Logo/Askari Housing Directorate.png';
+import c2 from '../images/Estimation & Management/Clients Logo/Bahria Enclave.png';
+import c3 from '../images/Estimation & Management/Clients Logo/Bahria Town Lahore, Karachi, Rawalpindi, Islamabad.jpg';
+import c4 from '../images/Estimation & Management/Clients Logo/Bahria.png';
+import c5 from '../images/Estimation & Management/Clients Logo/BWC.JPG';
+import c6 from '../images/Estimation & Management/Clients Logo/Capital Smart City.png';
+import c7 from '../images/Estimation & Management/Clients Logo/CDA.jpg';
+import c8 from '../images/Estimation & Management/Clients Logo/City Housing.jpg';
+import c9 from '../images/Estimation & Management/Clients Logo/Dha City Karachi.png';
+import c10 from '../images/Estimation & Management/Clients Logo/Dha Gujranwala.jpg';
+import c11 from '../images/Estimation & Management/Clients Logo/Dha Lahore.png';
+import c12 from '../images/Estimation & Management/Clients Logo/Dha Multan.png';
+import c13 from '../images/Estimation & Management/Clients Logo/Dha Peshawar.png';
+import c14 from '../images/Estimation & Management/Clients Logo/Dha quetta.jpg';
+import c15 from '../images/Estimation & Management/Clients Logo/Gharana.png';
+import c16 from '../images/Estimation & Management/Clients Logo/Green Oaks.jpg';
+import c17 from '../images/Estimation & Management/Clients Logo/Gulberg Greens.png';
+import c18 from '../images/Estimation & Management/Clients Logo/Gwadar Golf City.jpg';
+import c19 from '../images/Estimation & Management/Clients Logo/Karachi Development Authority.png';
+import c20 from '../images/Estimation & Management/Clients Logo/Kingdom Valley.png';
+import c21 from '../images/Estimation & Management/Clients Logo/Lahore Smart City.jpg';
+import c22 from '../images/Estimation & Management/Clients Logo/Lahre Development Authority.jpg';
+import c23 from '../images/Estimation & Management/Clients Logo/Multan Development Authority.jpg';
+import c24 from '../images/Estimation & Management/Clients Logo/Naya Pakistan Housing Program.png';
+import c25 from '../images/Estimation & Management/Clients Logo/New Metro City.png';
+import c26 from '../images/Estimation & Management/Clients Logo/Nova.jpg';
+import c27 from '../images/Estimation & Management/Clients Logo/Pakistan Defence Officers housing Authority.png';
+import c28 from '../images/Estimation & Management/Clients Logo/Pakistan Defence.png';
+import c29 from '../images/Estimation & Management/Clients Logo/Park View City.png';
+import c30 from '../images/Estimation & Management/Clients Logo/Rda.png';
+import c31 from '../images/Estimation & Management/Clients Logo/Zaraj.jpg';
 const ClientsData = () => {
   return [
     {
       key: 1,
-      img: DHA,
+      img: c1,
     },
     {
       key: 2,
-      img: DHA,
+      img: c2,
     },
     {
       key: 3,
-      img: DHA,
+      img: c3,
     },
     {
       key: 4,
-      img: DHA,
+      img: c4,
     },
     {
       key: 5,
-      img: DHA,
+      img: c5,
     },
     {
       key: 6,
-      img: DHA,
+      img: c6,
     },
     {
       key: 7,
-      img: DHA,
+      img: c7,
     },
     {
       key: 8,
-      img: DHA,
+      img: c8,
+    },
+    {
+      key: 9,
+      img: c9,
+    },
+    {
+      key: 10,
+      img: c10,
+    },
+    {
+      key: 11,
+      img: c11,
+    },
+    {
+      key: 12,
+      img: c12,
+    },
+    {
+      key: 13,
+      img: c13,
+    },
+    {
+      key: 14,
+      img: c14,
+    },
+    {
+      key: 15,
+      img: c15,
+    },
+    {
+      key: 16,
+      img: c16,
+    },
+    {
+      key: 17,
+      img: c17,
+    },
+    {
+      key: 18,
+      img: c18,
+    },
+    {
+      key: 19,
+      img: c19,
+    },
+    {
+      key: 20,
+      img: c20,
+    },
+    {
+      key: 21,
+      img: c21,
+    },
+    {
+      key: 22,
+      img: c22,
+    },
+    {
+      key: 23,
+      img: c23,
+    },
+    {
+      key: 24,
+      img: c24,
+    },
+    {
+      key: 25,
+      img: c25,
+    },
+    {
+      key: 26,
+      img: c26,
+    },
+    {
+      key: 27,
+      img: c27,
+    },
+    {
+      key: 28,
+      img: c28,
+    },
+    {
+      key: 29,
+      img: c29,
+    },
+    {
+      key: 30,
+      img: c30,
+    },
+    {
+      key: 31,
+      img: c31,
     },
   ];
 };
