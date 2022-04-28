ad0084d41c81fd900124cc5156593c0648ef8c28
MalikMuneeb20 <101018722+MalikMuneeb20@users.noreply.github.com>
2022-04-28 20:55:03 +0500

    Services Module added

--- src/components/Contractor & Consultancy/Homepage/Services/Services.js
+++ src/components/Contractor & Consultancy/Homepage/Services/Services.js
@@ -1,21 +1,22 @@
 import React from 'react';
-
-import exterior from "../../../../images/Estimation & Management/ser/exterior.jpg";
+import ServicesCard from '../../Cards/ServicesCard';
 const Services = () => {
   return <div>
-    <div style={{ display: "flex",height:"50vh",width:"80%",marginLeft:"auto",marginRight:"auto" }}>
+   
+    <ServicesCard/>
+    {/* <div style={{ display: "flex",flexDirection:"row-reverse",height:"50vh",width:"80%",marginLeft:"auto",marginRight:"auto" }}>
       <div style={{ display: "flex", width: "50%", alignItems: "center", justifyContent: "right" }}>
         <div style={{display:"flex",alignItems:"center",backgroundPosition: "center",backgroundRepeat: "no-repeat",backgroundSize: "cover",height:"40vh",width:"100%", backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url(${exterior})`,borderRadius: "9px"}} ></div>
       </div>
-      <div style={{ display: "flex", width: "50%", alignItems: "center", justifyContent: "right" }}>
+      <div style={{ display: "flex", width: "50%", alignItems: "center", justifyContent: "left" }}>
         <div style={{width:"80%",textAlign:"justify"}}>
-          <h1 style={{fontSize:"15px"}}>Architect & Interiors</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus tortor tincidunt, aliquam erat non, vulputate ligula. Vivamus auctor ut purus vitae venenatis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus tortor tincidunt, aliquam erat non, vulputate ligula. Vivamus auctor ut purus vitae venenatis.</p>
-          <a href='' style={{padding:"6px 15px",color:"#FFFFFF",textDecoration:"none",borderRadius:"4px",background:"linear-gradient(to bottom right, #FBCD38,#FCBB5B)"}}>View More</a>
+          <h1 style={{fontSize:"35px"}}>Architect & Interiors</h1>
+          <p style={{fontSize:"15px"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus tortor tincidunt, aliquam erat non, vulputate ligula. Vivamus auctor ut purus vitae venenatis.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempus tortor tincidunt, aliquam erat non, vulputate ligula. Vivamus auctor ut purus vitae venenatis.</p>
+          <a href='' style={{padding:"6px 15px",fontSize:"15px",color:"#FFFFFF",textDecoration:"none",borderRadius:"4px",background:"linear-gradient(to bottom right, #FBCD38,#FCBB5B)"}}>View More</a>
         </div>
       </div>
-
-    </div>
+    </div> */}
+    
   </div>;
 };
 
